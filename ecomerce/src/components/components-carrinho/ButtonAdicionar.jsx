4e136c28be25576fb7383ad37bf90719b8398139
Maria Aragao <maria.s.aragao.2025@gmail.com>
2025-06-12 03:01:46 -0300

    Desenvolvido botão que conecta com a API e adiciona ou remove um item do carrinho

---
+++ ecomerce/src/components/components-carrinho/ButtonAdicionar.jsx
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { useState } from "react";
+import { BsCartPlus } from "react-icons/bs";
+import { BsCartCheckFill } from "react-icons/bs";
+
+export function ButtonAdicionar({ idProduto }) {
+  const [adicionado, setAdicionado] = useState(false);
+  const token = localStorage.getItem("token");
+
+  const handleClick = async () => {
+    if (adicionado) {
+      const sucesso = await removerProduto();
+      if (sucesso) setAdicionado(false);
+    } else {
+      const sucesso = await adicionarProduto();
+      if (sucesso) setAdicionado(true);
+    }
+  };
+
+  const adicionarProduto = async () => {
+    try {
+      await axios.post(
+        `http://localhost:8080/pedidos/adicionar?idProduto=${idProduto}&quantidade=1`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      alert("Produto adicionado ao carrinho com sucesso!");
+      return true;
+    } catch (error) {
+      alert("Erro ao adicionar produto no carrinho!");
+      return false;
+    }
+  };
+
+  const removerProduto = async () => {
+    try {
+      await axios.post(
+        `http://localhost:8080/pedidos/excluir/item?idProduto=${idProduto}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      alert("Produto removido do carrinho!!");
+      return true;
+    } catch (error) {
+      alert("Erro ao remover produto no carrinho!");
+      return false;
+    }
+  };
+
+  return (
+    <button onClick={handleClick}>
+      {adicionado ? <BsCartCheckFill /> : <BsCartPlus />}
+    </button>
+  );
+}
