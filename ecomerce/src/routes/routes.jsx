a146af8b655ab03c79503db8990591edf8808911
Maria Aragao <maria.s.aragao.2025@gmail.com>
2025-06-13 20:06:43 -0300

    Rotas organizadas

--- ecomerce/src/routes/routes.jsx
+++ ecomerce/src/routes/routes.jsx
@@ -6,9 +6,13 @@ import { HomeFuncionario } from "../pages/homeFuncionario/HomeFuncionario";
 export function Rotas() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/homeFuncionario" element={<HomeFuncionario />} />
+      <Route path="/sobre" element={<About />} />
+      <Route path="/cadastro-produto" element={<CadastroProduto />} />
+      <Route path="/cadastro-categoria" element={<CadastroCategoria />} />
+      <Route path="/cadastroFuncionario" element={<CadastroFuncionario />} />
     </Routes>
   );
 }
