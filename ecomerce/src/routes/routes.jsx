ead7a8cf7503a148fda8507e43ac60c03ff4ea05
Lívia Raissinger <liviaraissinger@gmail.com>
2025-06-13 20:56:49 -0300

    Atualizacao Routes

--- ecomerce/src/routes/routes.jsx
+++ ecomerce/src/routes/routes.jsx
@@ -3,17 +3,19 @@ import { Login } from "../pages/login/login";
 import { HomeFuncionario } from "../pages/homeFuncionario/homeFuncionario";
 import { CadastroCategoria } from "../pages/homeFuncionario/cadastroCategoria";
 import { CadastroProduto } from "../pages/homeFuncionario/cadastroProduto";
-
+// import { About } from "../Pages/About/About";
+// import { CadastroFuncionario } from "../pages/cadastro-funcionario/cadastro-funcionario";
 
 export function Rotas() {
   return (
     <Routes>
-
+      {/* <Route path="/home" element={<Home />} /> */}
       < Route path="/login" element={<Login />} />
       <Route path="/homeFuncionario" element={<HomeFuncionario />} />
       <Route path="/cadastro-produto" element={<CadastroProduto />} />
       <Route path="/cadastro-categoria" element={<CadastroCategoria />} />
-
+      {/* <Route path="/sobre" element={<About />} />
+      <Route path="/CadastroFuncionario" element={<CadastroFuncionario />} /> */}
 
     </Routes>
   );
