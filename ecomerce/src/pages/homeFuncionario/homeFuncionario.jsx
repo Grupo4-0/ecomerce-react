dd1421e02883113305797bd7eb824aacd74c1550
Lívia Raissinger <liviaraissinger@gmail.com>
2025-06-13 21:17:49 -0300

    Atualizacao Home

--- ecomerce/src/pages/homeFuncionario/homeFuncionario.jsx
+++ ecomerce/src/pages/homeFuncionario/homeFuncionario.jsx
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./homeF.module.css"
 
 export function HomeFuncionario() {
-
+    const navigate = useNavigate()
+    //add botao para mandar para a home principal
     return (
-        <h1>Funcionou!!</h1>
+        <div className={styles.conteudo}>
+            <h1> Bem vindo(a)!</h1>
+            <div >
+                <h1>Painel do Funcionário</h1>
+                <p>Gerencie os produtos e categorias aqui.</p>
+                <h2> O que voce deseja cadastrar? </h2>
+                <div className={styles.botoes}>
+                    <button onClick={() => navigate("/cadastro-categoria")}>
+                        Categoria
+                    </button>
+                    <button onClick={() => navigate("/cadastro-produto")}>
+                        Produto
+                    </button>
+                </div>
+            </div>
+
+        </div>
     )
+
 }
