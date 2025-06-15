7fa602403956a38f1f20f3bdaf430117faafbf5f
José Carlos Boaretto Netto <boarettonetto@gmail.com>
2025-06-15 19:30:26 -0300

    finalizando o css

--- ecomerce/src/routes/routes.jsx
+++ ecomerce/src/routes/routes.jsx
@@ -6,6 +6,8 @@ import { Route, Routes } from "react-router-dom";
 // import { About } from "../Pages/About/About";
 // import { CadastroFuncionario } from "../pages/cadastro-funcionario/cadastro-funcionario";
 import { CadastroCliente } from "../pages/cadastro-cliente/cadastro-cliente";
+import { CadastroFuncionario } from "./../pages/cadastro-funcionario/cadastro-funcionario";
+import { Home } from "./../pages/home/home";
 export function Rotas() {
   return (
     <Routes>
@@ -16,7 +18,9 @@ export function Rotas() {
       <Route path="/cadastro-categoria" element={<CadastroCategoria />} />
       <Route path="/sobre" element={<About />} />
       <Route path="/cadastroFuncionario" element={<CadastroFuncionario />} /> */}
+      <Route path="/cadastroFuncionario" element={<CadastroFuncionario />} />
       <Route path="/cliente/cadastro" element={<CadastroCliente />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
