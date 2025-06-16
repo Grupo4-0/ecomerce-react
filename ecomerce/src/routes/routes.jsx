07db7dd4a88d25e6379d6757c5b40fa3347d657a
José Carlos Boaretto Netto <boarettonetto@gmail.com>
2025-06-16 17:35:40 -0300

    Alteracoes do funcionarioHome

--- ecomerce/src/routes/routes.jsx
+++ ecomerce/src/routes/routes.jsx
@@ -3,18 +3,19 @@ import { Login } from "../pages/login/login";
 import { Home } from "../pages/home/home";
 import { CadastroCliente } from "../pages/cadastro-cliente/cadastro-cliente";
 import { CadastroFuncionario } from "../pages/cadastro-funcionario/cadastro-funcionario";
-import { funcionarioHome } from "./../pages/funcionarioHome/funcionarioHome";
-import { CadastroCategoria } from "./../pages/funcionarioHome/cadastroCategoria";
-import { CadastroProduto } from "../pages/funcionarioHome/cadastroProduto";
+import { FuncionarioHome } from "./../pages/homeFuncionario/funcionarioHome";
+import { CadastroProduto } from "./../pages/homeFuncionario/cadastroProduto";
+import { CadastroCategoria } from "./../pages/homeFuncionario/cadastroCategoria";
+
 export function Rotas() {
   return (
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/funcionarioHome" element={<funcionarioHome />} />
+      <Route path="/funcionarioHome" element={<FuncionarioHome />} />
       <Route path="/cadastro-produto" element={<CadastroProduto />} />
       <Route path="/cadastro-categoria" element={<CadastroCategoria />} />
-      <Route path="/sobre" element={<About />} />
+      {/* <Route path="/sobre" element={<About />} /> */}
       <Route path="/cliente/cadastro" element={<CadastroCliente />} />
       <Route path="/funcionario/cadastro" element={<CadastroFuncionario />} />
     </Routes>
