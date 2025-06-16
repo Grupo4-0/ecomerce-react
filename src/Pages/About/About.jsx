f2a0f84e7ef29321be357c939d68c867751b2ac9
Thais Costa <thais1fc2002@gmail.com>
2025-06-16 14:49:32 -0300

    Colocando  as mensagens

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -10,48 +10,48 @@ import imgPaulo from "../../assets/PauloRicardo.png";
 
 function About() {
   return (
-    <div className="poppins-thin ">
+    <div className=".poppins-regular-italic ">
       <h1 style={{ textAlign: "center", marginTop: "20px" }}>Sobre a Equipe</h1>
       <div className="grid-container">
         <ProfileFlipCard
           image={imgMaria}
           name="Maria Araguão"
-          quote="Mensagem Inspiradora "
+          quote="Você não precisa saber tudo. Só precisa continuar aprendendo um pouco a cada dia."
           description=" Sua descrição "
           githubLink="https://github.com/Maria-S-Aragao"
         />
         <ProfileFlipCard
           image={imgJose}
           name="José Netto"
-          quote="Mensagem Inspiradora "
+          quote="Todo programador já se sentiu perdido. A diferença está em quem decide continuar. "
           description=" Sua descrição "
           githubLink="https://github.com/jcboaretto1"
         />
         <ProfileFlipCard
           image={imgLivia}
           name="Lívia Raissiger"
-          quote="Mensagem Inspiradora "
-          description=" Sua descrição "
+          quote="Grandes sistemas começam com pequenos comandos. Continue codando. "
+          description=" Sou apaixonada pela área da tecnologia e, no momento, tenho mais interesse em front-end. Tenho facilidade para trabalhar em equipe, sou organizada, dedicada e estou sempre em busca de aprendizado para evoluir tecnicamente. "
           githubLink="https://github.com/Livia9"
         />
         <ProfileFlipCard
           image={imgNathan}
           name="Nathan Gomes"
-          quote="Mensagem Inspiradora "
+          quote="Cada linha que você escreve te aproxima do programador que você quer ser."
           description=" Sua descrição "
           githubLink="https://github.com/NathandGomes"
         />
         <ProfileFlipCard
           image={imgThais}
           name="Thais Costa"
-          quote="Não é só sobre programar. É sobre resolver problemas, aprender todos os dias e nunca parar de evoluir."
+          quote="Vocês tem sorte de Não torcer para o Vasco."
           description=" Tenho 22 anos, sou estudante de Software e apaixonada por tecnologia. Acredito que a programação é uma ferramenta poderosa para transformar o mundo."
           githubLink="https://github.com/tatacost"
         />
         <ProfileFlipCard
           image={imgPaulo}
           name="Paulo Ricardo"
-          quote="Mensagem Inspiradora "
+          quote="Errar faz parte do código. Cada bug é um passo a mais rumo à maestria."
           description=" Sua descrição "
           githubLink="https://github.com/pauloricardoc"
         />
