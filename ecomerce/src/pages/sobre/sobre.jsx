030ceb53634231aa2e53925e0145aaf0b25fd937
Thais Costa <thais1fc2002@gmail.com>
2025-06-16 17:29:38 -0300

    colocando o sobre

--- ecomerce/src/pages/sobre/sobre.jsx
+++ ecomerce/src/pages/sobre/sobre.jsx
@@ -1,3 +1,63 @@
-export function Sobre() {
-  return;
+import { useState } from "react";
+import "../../pages/sobre/sobre.css";
+import ProfileFlipCard from "../../components/ProfileFlipCard/ProfileFlipCard";
+import imgMaria from "../../assets/MariaAraguao.png";
+import imgJose from "../../assets/JoseNetto.png";
+import imgLivia from "../../assets/Livia.png";
+import imgNathan from "../../assets/NathanGomes.png";
+import imgThais from "../../assets/Thais.png";
+import imgPaulo from "../../assets/PauloRicardo.png";
+
+function Sobre() {
+  return (
+    <div className="poppins-regular-italic ">
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>Sobre a Equipe</h1>
+      <div className="grid-container-sobre">
+        <ProfileFlipCard
+          image={imgMaria}
+          name="Maria Araguão"
+          quote="Você não precisa saber tudo. Só precisa continuar aprendendo um pouco a cada dia."
+          description=" Sua descrição "
+          githubLink="https://github.com/Maria-S-Aragao"
+        />
+        <ProfileFlipCard
+          image={imgJose}
+          name="José Netto"
+          quote="Todo programador já se sentiu perdido. A diferença está em quem decide continuar. "
+          description=" Sua descrição "
+          githubLink="https://github.com/jcboaretto1"
+        />
+        <ProfileFlipCard
+          image={imgLivia}
+          name="Lívia Raissiger"
+          quote="Grandes sistemas começam com pequenos comandos. Continue codando. "
+          description=" Sou apaixonada pela área da tecnologia e, no momento, tenho mais interesse em front-end. Tenho facilidade para trabalhar em equipe, sou organizada, dedicada e estou sempre em busca de aprendizado para evoluir tecnicamente. "
+          githubLink="https://github.com/Livia9"
+        />
+        <ProfileFlipCard
+          image={imgNathan}
+          name="Nathan Gomes"
+          quote="Cada linha que você escreve te aproxima do programador que você quer ser."
+          description=" Sua descrição "
+          githubLink="https://github.com/NathandGomes"
+        />
+        <ProfileFlipCard
+          image={imgThais}
+          name="Thais Costa"
+          quote="Vocês tem sorte de Não torcer para o Vasco."
+          description=" Tenho 22 anos, sou estudante de Software e apaixonada por tecnologia. Acredito que a programação é uma ferramenta poderosa para transformar o mundo."
+          githubLink="https://github.com/tatacost"
+        />
+        <ProfileFlipCard
+          image={imgPaulo}
+          name="Paulo Ricardo"
+          quote="Errar faz parte do código. Cada bug é um passo a mais rumo à maestria."
+          description=" Sua descrição "
+          githubLink="https://github.com/pauloricardoc"
+        />
+      </div>
+    </div>
+  );
 }
+
+export default Sobre;
--- ecomerce/src/routes/routes.jsx
+++ ecomerce/src/routes/routes.jsx
@@ -3,13 +3,14 @@ import { Login } from "../pages/login/login";
 import { HomeFuncionario } from "../pages/homeFuncionario/homeFuncionario";
 // import { CadastroCategoria } from "../pages/homeFuncionario/cadastroCategoria";
 // import { CadastroProduto } from "../pages/homeFuncionario/cadastroProduto";
-// import { About } from "../Pages/About/About";
+
 {
   /* <Route path="/CadastroFuncionario" element={<CadastroFuncionario />} /> */
 }
 import { Home } from "../pages/home/home";
 import { CadastroCliente } from "../pages/cadastro-cliente/cadastro-cliente";
 import { CadastroFuncionario } from "../pages/cadastro-funcionario/cadastro-funcionario";
+import Sobre from "../pages/sobre/sobre";
 export function Rotas() {
   return (
     <Routes>
@@ -18,8 +19,9 @@ export function Rotas() {
       <Route path="/homeFuncionario" element={<HomeFuncionario />} />
       {/* <Route path="/cadastro-produto" element={<CadastroProduto />} />
       <Route path="/cadastro-categoria" element={<CadastroCategoria />} />
-=======
-      <Route path="/sobre" element={<About />} /> */}
+=======*/}
+
+      <Route path="/sobre" element={<Sobre />}></Route>
       <Route path="/cliente/cadastro" element={<CadastroCliente />} />
       <Route path="/funcionario/cadastro" element={<CadastroFuncionario />} />
     </Routes>
