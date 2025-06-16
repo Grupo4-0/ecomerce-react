523950069fe1ddc1d89303b774c9f69d7d6d5697
Thais Costa <thais1fc2002@gmail.com>
2025-06-16 18:59:10 -0300

    terminado

--- ecomerce/src/pages/sobre/sobre.jsx
+++ ecomerce/src/pages/sobre/sobre.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../../pages/sobre/sobre.module.css";
 import ProfileFlipCard from "../../components/ProfileFlipCard/ProfileFlipCard";
 import imgMaria from "../../assets/MariaAraguao.png";
@@ -7,25 +8,45 @@ import imgLivia from "../../assets/Livia.png";
 import imgNathan from "../../assets/NathanGomes.png";
 import imgThais from "../../assets/Thais.png";
 import imgPaulo from "../../assets/PauloRicardo.png";
+import { Navbar } from "../../components/Navbar/navbar";
 
 function Sobre() {
+  const navigate = useNavigate();
   return (
     <>
       <div className={styles.divSobre}>
         <h1 style={{ textAlign: "center", marginTop: "20px" }}>Sobre a Equipe</h1>
+
+        <div style={{ textAlign: "center", marginBottom: "20px" }}>
+          <button
+            onClick={() => navigate("/")}
+            style={{
+              backgroundColor: "#ff9ac1",
+              border: "none",
+              padding: "10px 20px",
+              borderRadius: "10px",
+              fontSize: "16px",
+              cursor: "pointer",
+              boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
+            }}
+          >
+            Voltar para Home
+          </button>
+        </div>
+
         <div className={styles.gridSobre}>
           <ProfileFlipCard
             image={imgMaria}
             name="Maria Araguão"
             quote="Eu não falhei. Apenas descobri 10.000 maneiras que não funcionam."
-            description=" Tenh"
+            description="Estou sempre estudando, testando novas tecnologias e buscando escrever um backend cada vez mais inteligente, escalável e sustentável. Se tiver lógica envolvida, estou dentro. Se for pra resolver um bug desafiador, melhor ainda. "
             githubLink="https://github.com/Maria-S-Aragao"
           />
           <ProfileFlipCard
             image={imgJose}
             name="José Netto"
             quote="Todo programador já se sentiu perdido. A diferença está em quem decide continuar. "
-            description=" Sua descrição "
+            description="Sou uma pessoa colaborativa, paciente e que valoriza o trabalho em equipe. Tenho interesse em atuar na área de tecnologia, com foco em backend Java. Gosto de ambientes leves e estou sempre disposto a contribuir, aprender e crescer junto com o time."
             githubLink="https://github.com/jcboaretto1"
           />
           <ProfileFlipCard
@@ -38,8 +59,8 @@ function Sobre() {
           <ProfileFlipCard
             image={imgNathan}
             name="Nathan Gomes"
-            quote="Cada linha que você escreve te aproxima do programador que você quer ser."
-            description=" Sua descrição "
+            quote="Transformo café em código e ideias em soluções."
+            description=" Trabalho com foco em eficiência, segurança e clareza no código. Curto resolver problemas complexos, otimizar processos e construir soluções sólidas, pensando sempre na escalabilidade e na manutenção a longo prazo. "
             githubLink="https://github.com/NathandGomes"
           />
           <ProfileFlipCard
@@ -53,7 +74,7 @@ function Sobre() {
             image={imgPaulo}
             name="Paulo Ricardo"
             quote="Errar faz parte do código. Cada bug é um passo a mais rumo à maestria."
-            description=" Sua descrição "
+            description=" Sou movido a desafios. Gosto de pegar problemas complexos, quebrar em partes e construir soluções inteligentes e eficientes. Trabalhar com lógica, segurança, performance e escalabilidade não só me motiva, como me diverte. "
             githubLink="https://github.com/pauloricardoc"
           />
         </div>
